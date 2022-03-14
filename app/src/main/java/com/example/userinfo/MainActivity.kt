f9737161088fbfe27e5cd2ce4bcba558858a9ce5
Charan <sai.peram@everest.engineering>
2022-03-14 23:01:25 +0530

    added fragment skeletons for user input and display info

--- app/src/main/java/com/example/userinfo/MainActivity.kt
+++ app/src/main/java/com/example/userinfo/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userInputFragment = UserInputFragment()
+        val userInputTransaction = supportFragmentManager.beginTransaction()
+        userInputTransaction.replace(R.id.main_container, userInputFragment)
+        userInputTransaction.commit()
+
     }
 }
