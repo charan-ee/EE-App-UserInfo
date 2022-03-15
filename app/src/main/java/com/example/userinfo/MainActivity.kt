040693c44402279f4d67438acfe9e96263c152e5
Charan <sai.peram@everest.engineering>
2022-03-15 15:29:27 +0530

    added validators to validate input fields

--- app/src/main/java/com/example/userinfo/MainActivity.kt
+++ app/src/main/java/com/example/userinfo/MainActivity.kt
@@ -11,9 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val userInputFragment = UserInputFragment()
+
         val userInputTransaction = supportFragmentManager.beginTransaction()
         userInputTransaction.replace(R.id.main_container, userInputFragment)
         userInputTransaction.commit()
-
     }
 }
--- app/src/main/java/com/example/userinfo/UserInputFragment.kt
+++ app/src/main/java/com/example/userinfo/UserInputFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputBinding
+import androidx.lifecycle.ViewModelProvider
 import com.example.userinfo.databinding.FragmentUserInputBinding
 import com.example.userinfo.model.UserViewModel
 
@@ -13,25 +14,25 @@ class UserInputFragment : Fragment() {
 
     private var fragmentUserInputBinding: FragmentUserInputBinding? = null
     private lateinit var userViewModel: UserViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var displayInfoFragment: DisplayInfoFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val userInputFragment = FragmentUserInputBinding.inflate(inflater, container, false)
-        fragmentUserInputBinding = userInputFragment
-        return userInputFragment.root
+        fragmentUserInputBinding = FragmentUserInputBinding.inflate(inflater, container, false)
+//        fragmentUserInputBinding = userInputFragment
+        return fragmentUserInputBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        userViewModel = UserViewModel()
+        displayInfoFragment = DisplayInfoFragment()
+
+        userViewModel = activity?.let { ViewModelProvider(it)[UserViewModel::class.java] }
+            ?: throw RuntimeException("Not a Activity")
+
 
 
     }
